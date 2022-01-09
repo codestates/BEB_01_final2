b17a526f9d96a7e7da4756f008b1cb3da73c8750
201524554 <43158434+201524554@users.noreply.github.com>
2022-01-10 02:01:20 +0900

    erc20 수정

--- contract/Temp.sol
+++ contract/Temp.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.10;
-import "../libraries/Token.sol";
+import "./Token.sol";
 
-contract Temp is Token{
+contract Temp is Token("gold", "GOLD"){
 
-    
+    constructor() {
+
+    }
 }
