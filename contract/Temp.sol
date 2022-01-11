5f5c9b3124951493e7afa220f04c97896c3a0947
201524554 <43158434+201524554@users.noreply.github.com>
2022-01-12 05:40:22 +0900

    erc20 erc721 합치기

--- contract/Temp.sol
+++ contract/Temp.sol
@@ -3,28 +3,38 @@ pragma solidity 0.8.10;
 import "./libraries/Token.sol";
 import "./libraries/NFT.sol";
 
-contract Temp is Token("gold", "GOLD"), NFT("item", "ITM"){
+contract Temp is NFT("item", "ITM"){
 
-    constructor() {
+    Token private gold;
 
+    constructor(address token) {
+        gold = Token(token);
     }
-
-    function name() public override(Token, NFT) view returns(string memory){
-        return Token.name();
-    }
-
-    function symbol() public override(Token, NFT) view returns(string memory){
-        return Token.symbol();
-    }
-
-    function NFTname() public view returns(string memory){
-        return super.name();
-    }
-
-    function NFTsymbol() public view returns(string memory){
-        return super.symbol();
-    }
-
     
+    function goldTotalSupply() public view returns(uint256){
+        return gold.totalSupply();
+    }
+    
+    function goldBalanceOf(address account) public view returns (uint256) {
+        return gold.balanceOf(account);
+    }
+
+    function goldCheck(address account) public view returns(bool){
+        return gold.check(account);
+    }
+
+    function goldMint(address to, uint256 amount) public{
+        gold.mintGold(amount, to);
+    }
+
+    function goldTransfer(address from, address to, uint256 amount)public {
+        gold.transfer(from, to, amount);
+    }
+
+    function goldBurn(address account, uint256 amount) public{
+        gold.burn(account, amount);
+    }
+
+
 
 }
