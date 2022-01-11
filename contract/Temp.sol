3d399a2b164b8de2705cd96c1361f902c3070b38
201524554 <43158434+201524554@users.noreply.github.com>
2022-01-11 13:49:48 +0900

    erc721 기능추가

--- contract/Temp.sol
+++ contract/Temp.sol
@@ -1,10 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.10;
-import "./Token.sol";
+import "./libraries/Token.sol";
+import "./libraries/NFT.sol";
 
-contract Temp is Token("gold", "GOLD"){
+contract Temp is Token("gold", "GOLD"), NFT("item", "ITM"){
 
     constructor() {
 
     }
+
+    function name() public override(Token, NFT) view returns(string memory){
+        return Token.name();
+    }
+
+    function symbol() public override(Token, NFT) view returns(string memory){
+        return Token.symbol();
+    }
+
+    function NFTname() public view returns(string memory){
+        return super.name();
+    }
+
+    function NFTsymbol() public view returns(string memory){
+        return super.symbol();
+    }
+
+    
+
 }
