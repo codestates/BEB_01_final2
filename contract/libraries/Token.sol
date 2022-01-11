ecf12d50694c9edb4d8fc7e4ae4856b1c980c7f5
201524554 <43158434+201524554@users.noreply.github.com>
2022-01-12 05:38:17 +0900

    erc20 erc721 합치기

--- contract/libraries/Token.sol
+++ contract/libraries/Token.sol
@@ -2,24 +2,18 @@
 pragma solidity 0.8.10;
 
 interface TokenInterface {
-    function GoldTotalSupply() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address from, address recipient, uint256 amount) external returns (bool);
 
     event Transfer(address indexed from, address indexed to, uint256 amount);
-    event Transfer(address indexed spender, address indexed from, address indexed to, uint256 amount);
-    event Approval(address indexed owner, address indexed spender, uint256 amount);
 }
 
 contract Token is TokenInterface {
-    mapping(address => uint256) private _GoldBalances;
-    mapping(address => mapping(address => uint256)) private _allowances;
+    mapping(address => uint256) private _balances;
     mapping(address => bool) private _check;
 
-    uint256 private _GoldTotalSupply;
+    uint256 private _totalSupply;
     string private _name;
     string private _symbol;
     uint256 private _decimals;
@@ -33,21 +27,16 @@ contract Token is TokenInterface {
         _symbol = TokenSymbol_;
     }
 
-    function GoldTotalSupply() public view returns (uint256) {
-        return _GoldTotalSupply;
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
     }
 
-    function balanceOf(address account) public view returns (uint256) {
-        return _GoldBalances[account];
+    function balanceOf(address account) public virtual view returns (uint256) {
+        return _balances[account];
     }
 
     function decimals() public view virtual returns (uint8) {
         return 0;
-    }
-
-    function allowance(address owner, address spender)public view returns (uint256)
-    {
-        return _allowances[owner][spender];
     }
 
     function name() public virtual view returns (string memory) {
@@ -70,14 +59,15 @@ contract Token is TokenInterface {
     }
 
     function _mintGold(uint256 amount, address to) internal {
-        _GoldBalances[to] += amount;
-        _GoldTotalSupply += amount;
-        emit Transfer(address(0), msg.sender, amount);
+        _balances[to] += amount;
+        _totalSupply += amount;
+        emit Transfer(address(0), to, amount);
     }
 
-    function transfer(address recipient, uint amount) public virtual override isMint(msg.sender) returns (bool) {
-        _transfer(msg.sender, recipient, amount);
-        emit Transfer(msg.sender, recipient, amount);
+    function transfer(address from, address recipient, uint amount) public virtual override isMint(from) isMint(recipient) 
+    returns (bool) {
+        _transfer(from, recipient, amount);
+        emit Transfer(from, recipient, amount);
         return true;
     }
 
@@ -86,56 +76,23 @@ contract Token is TokenInterface {
     function _transfer(address sender, address recipient, uint256 amount) internal {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
-        //uint256 senderBalance = _GoldBalances[sender];
-        require(
-            _GoldBalances[sender] >= amount,
-            "ERC20: transfer amount exceeds balance"
-        );
-        // 일단 goldBalances를 갱신해 준다.
-        _GoldBalances[sender] -= amount;
-        _GoldBalances[recipient] += amount;
-        // 그후 _allowances값도 똑같이 갱신해 준다.
-        // _allowances[sender][checkCA] -= amount;
-        // _allowances[recipient][checkCA] += amount;
+        require(_balances[sender] >= amount, "ERC20: transfer amount exceeds balance");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
     }
 
-  
 
-    function _approve(address owner, address spender, uint256 amount) internal  returns (bool){
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
 
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-        return true;
-    }
-
-     function approve(address spender, uint amount) external virtual override isMint(msg.sender) isMint(msg.sender) returns (bool) {
-        uint256 currentAllownace = _allowances[msg.sender][spender];
-        require(currentAllownace >= amount, "ERC20: Transfer amount exceeds allowance");
-        _approve(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(address sender, address recipient, uint256 amount) external virtual override isMint(msg.sender)
-    returns (bool) {
-        _transfer(sender, recipient, amount);
-        emit Transfer(msg.sender, sender, recipient, amount);
-        uint256 currentAllowance = _allowances[sender][msg.sender];
-        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
-        _approve(sender, msg.sender, currentAllowance - amount);
-        return true;
-    }
-
-    function _burn(address account, uint256 amount) internal virtual isMint(msg.sender){
+    function _burn(address account, uint256 amount) internal virtual isMint(account){
         require(account != address(0), "ERC20: burn from the zero address");
 
-        uint256 accountBalance = _GoldBalances[account];
+        uint256 accountBalance = _balances[account];
         require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
         unchecked {
-            _GoldBalances[account] = accountBalance - amount;
+            _balances[account] = accountBalance - amount;
         }
-        _GoldTotalSupply -= amount;
+        _totalSupply -= amount;
 
         emit Transfer(account, address(0), amount);
     }
@@ -143,33 +100,5 @@ contract Token is TokenInterface {
     function burn(address account, uint256 amount) external{
         _burn(account, amount);
     }
-/*
-    function transferFrom(
-        address sender,
-        address checkCA,
-        address recipient,
-        uint256 amount
-    ) public returns (bool) {
-        uint256 currentAllowance = _allowances[sender][checkCA];
-        // 일단 전송사자 자신과 CA사이에 _allowances값이 있는지 확인
-        // 이 부분은 mintGold를 실행시킬떄에 _allowances[사용자][TokenCA] 구조를 만들어 주기 떄문에 민팅한 사용자만 해당 require문을 통과 가능
-        require(
-            currentAllowance >= amount,
-            "ERC20: transfer amount exceeds allowance"
-        );
-        _transfer(sender, recipient, checkCA, amount);
-        return true;
-    }
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-    */
    
 }
