a82d453a2eafb5a42f7ab5152e452bc871478cbf
201524554 <43158434+201524554@users.noreply.github.com>
2022-01-14 12:21:45 +0900

    auction 완성

--- contract/Auction.sol
+++ contract/Auction.sol
@@ -4,20 +4,14 @@ import "./libraries/NFT.sol";
 import "./libraries/Token.sol";
 
 contract Auction{
-
-    struct Bid{
-        bytes32 id;
-        uint deposit;
-    }
     
-    enum Phase {Init, Bidding, Open, Done}
-    
-    mapping(address => Bid) bid;
-    mapping(address => uint) Return;
+    enum Phase {Init, Bidding, Done}
 
     Phase public state = Phase.Init;
     address owner;
+    address seller;
     address public buyer;
+    uint public ID;
     uint public startPrice;
     uint public highestPrice = 0;
 
@@ -26,8 +20,8 @@ contract Auction{
         _;
     }
 
-    modifier isOwner(){
-        require(msg.sender == owner);
+    modifier isSeller(){
+        require(msg.sender == seller);
         _;
     }
 
@@ -37,21 +31,47 @@ contract Auction{
     constructor(address token, address nft, uint itemID, uint price){
         gold = Token(token);
         item = NFT(nft);
-        owner = item.ownerOf(itemID);
+        ID = itemID;
+        seller = item.ownerOf(ID);
         startPrice = price;
         state = Phase.Bidding;
     }
 
-    function nextState() public isOwner{
+    function nextState() public{
         if(state == Phase.Done) revert();
         if(state == Phase.Init)
             state = Phase.Bidding;
-        else if(state == Phase.Bidding)
-            state = Phase.Open;
-        else
+        else{
             state = Phase.Done;
+            trade();
+        }
+            
     }
-    
 
+    function Bid(uint price) public validPhase(Phase.Bidding){
+        uint balance = gold.balanceOf(msg.sender);
+        require(price < balance, "price exceeds balance");
+        require(price < highestPrice, "need to be higher than highest price");
+        buyer = msg.sender;
+    }
 
+    function highest() public view returns(uint256){
+        return highestPrice;
+    }
+
+    function showBuyer() public view returns(address){
+        return buyer;
+    }
+
+    function showSeller() public view returns(address){
+        return seller;
+    }
+
+    function trade() public validPhase(Phase.Done){
+        require(item.ownerOf(ID) == seller, "seller is not owner");
+        uint fee = highestPrice * 5 / 100;
+        gold.transfer(buyer, owner, fee);
+        gold.transfer(buyer, seller, highestPrice - fee);
+        item.transferFrom(seller, buyer, ID);
+    }
 }
